ed368b285ce2d2d2ab2cebc537e8b28e96188bab
emiliolo903 <emiliodaniellopez4@gmail.com>
2026-02-11 17:16:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,18 @@
-const CACHE_NAME = "tesla-poseada-v2";
-const urlsToCache = [
-  "index.html",
-  "manifest.json"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("tesla-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });

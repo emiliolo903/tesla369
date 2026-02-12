9d68ee44ea78d28d565d50ac8be3667f34cdf6c1
emiliolo903 <emiliodaniellopez4@gmail.com>
2026-02-12 05:35:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,8 @@
 const CACHE="emi-tesla-pro-v1";
+const urls=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll([
-"./",
-"./index.html",
-"./manifest.json"
-]))
-);
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls)));
 });
 self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });

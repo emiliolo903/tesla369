ac28d834d095b5567a82c44fe1b8aaf099e813f2
emiliolo903 <emiliodaniellopez4@gmail.com>
2026-07-24 15:26:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,11 @@ self.addEventListener("install",e=>{
 self.addEventListener("fetch",e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
+
+
+
+self.addEventListener(
+"fetch",
+function(event){
+
+});
